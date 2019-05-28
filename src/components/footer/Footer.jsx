3ef513b3307ccdbe497219263a21a4328f53db0f
Anatoly Sablin <sablintolya@gmail.com>
2019-05-28 23:01:17 +0300

    Initial footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,12 +4,28 @@ import {connect} from 'react-redux';
 class Footer extends React.PureComponent {
 
     render = () => (
-        <>
-            <div className="medium-blue"/>
-            <div className="row-full footer">
+        <div className="footer">
+            <div className="footer-emblem"/>
+            <div className="services">
+                <div className="title">Помощь</div>
+                <div className="title">Сервисы</div>
+                <div className="title">Клиенты</div>
+                <div className="footer-menu-column">
+                    <div>FAQ</div>
+                    <div>Contacts</div>
+                </div>
+                <div className="footer-menu-column">
+                    <div>Рейтинг конференций</div>
+                    <div>Архив сообщений</div>
+                    <div>Juick</div>
+                </div>
+                <div className="footer-menu-column">
+                    <div>chat.jabber.ru</div>
+                    <div>Xabber</div>
+                    <div>Conversations</div>
+                </div>
             </div>
-            <div className="medium-blue"/>
-        </>
+        </div>
     );
 }
 
